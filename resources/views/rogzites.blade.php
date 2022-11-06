2e053a5fe017ea44794e1a3d4c8e71011bbf9968
GruBen03 <harcsapergeto@gmail.com>
2022-11-06 20:13:01 +0100

    készül a pacekja a regisztracionak

---
+++ resources/views/rogzites.blade.php
@@ -0,0 +1,15 @@
+<div class="container bg-dark text-white rounded mt-5 text-center p-3">
+    <div class="row">
+        <div class="col">
+            <H1>Üdvözöllek az első Házidolgozatomban<br><h4>("tölts ki mindent!!444!44)</h4></H1>
+
+        </div>  
+    </div>
+</div>
+<div class="container bg-success text-white rounded text-center p-3">
+    <div class="row">
+        <div class="col">
+            <h2></h2>
+        </div>  
+    </div>
+</div>
